e3beb06d4f464cf4011c2f4804978f3b2b3625ef
Redline30 <39278564+Redline30@users.noreply.github.com>
2020-05-31 21:21:33 -0300

    particlesjs

--- script/main.js
+++ script/main.js
@@ -1,36 +1,153 @@
- new fullpage('#fullpage', {
-     //  {──────────────────────────────────────────────────::::::Opciones Basicas──────────────────────────────────────────────────
-     autoScrolling: true, // Se activa el scroll.
-     scrollBar: true,
-     fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
-     fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
-     easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
-     scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
-     css3: true, // Si usara CSS3 o javascript.
-     easingcss3: 'ease-out', // Curva de velocidad del efecto.
-     loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
-     //  ──────────────────────────────────────────────────::::::Barra de navegación──────────────────────────────────────────────────
-     navigation: false, // Muesta la barra de navegación.
-     menu: '#menu', // Menu de navegación.
-     anchors: ['home', 'aboutme', 'skills', 'footer'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
-     responsiveWidth: 767,
- });
+// PARTICLES.JS
 
- // Mobile Menu
+particlesJS('particles-js',
 
- console.log(document.querySelector('.menu-options-mobile'));
+    {
+        "particles": {
+            "number": {
+                "value": 28,
+                "density": {
+                    "enable": true,
+                    "value_area": 2051.7838682439087
+                }
+            },
+            "color": {
+                "value": "#a2ceff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.9627601227913727,
+                "random": true,
+                "anim": {
+                    "enable": true,
+                    "speed": 1,
+                    "opacity_min": 0,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 4,
+                    "size_min": 0.3,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": false,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 1,
+                "direction": "none",
+                "random": true,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 600
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": false,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 335.6643356643357,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 179.8201798201798,
+                    "size": 0,
+                    "duration": 1.838161838161838,
+                    "opacity": 0,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 135.86413586413587,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
 
- const menum = document.querySelector('.menu-options-mobile');
- console.log(menum);
- const burger = document.querySelector('.burgerbutton');
- console.log(burger);
+// FULLPAGE.js
 
- burger.addEventListener('click', hideShow);
+new fullpage('#fullpage', {
+    //  {──────────────────────────────────────────────────::::::Opciones Basicas──────────────────────────────────────────────────
+    autoScrolling: true, // Se activa el scroll.
+    scrollBar: true,
+    fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+    fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+    easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
+    scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
+    css3: true, // Si usara CSS3 o javascript.
+    easingcss3: 'ease-out', // Curva de velocidad del efecto.
+    loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+    //  ──────────────────────────────────────────────────::::::Barra de navegación──────────────────────────────────────────────────
+    navigation: false, // Muesta la barra de navegación.
+    menu: '#menu', // Menu de navegación.
+    anchors: ['home', 'aboutme', 'skills', 'footer'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+    responsiveWidth: 767,
+});
 
- function hideShow() {
-     if (menum.classList.contains('is-active')) {
-         menum.classList.remove('is-active');
-     } else {
-         menum.classList.add('is-active');
-     }
- }
+// Mobile Menu
+
+console.log(document.querySelector('.menu-options-mobile'));
+
+const menum = document.querySelector('.menu-options-mobile');
+console.log(menum);
+const burger = document.querySelector('.burgerbutton');
+console.log(burger);
+
+burger.addEventListener('click', hideShow);
+
+function hideShow() {
+    if (menum.classList.contains('is-active')) {
+        menum.classList.remove('is-active');
+    } else {
+        menum.classList.add('is-active');
+    }
+}
